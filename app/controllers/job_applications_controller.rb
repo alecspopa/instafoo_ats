abaa8d362bf321fc09cc597a3c761ba95f70476b
Alecs Popa <alecs.popa@gmail.com>
2025-06-15 23:45:39 +0200

    Fix n+1 in job_applications_controller

--- app/controllers/job_applications_controller.rb
+++ app/controllers/job_applications_controller.rb
@@ -2,7 +2,7 @@ class JobApplicationsController < ApplicationController
   # GET /job_applications
   def index
     @job_applications = JobApplication
-      .includes(:current_event, :job_application_events, job: [ current_event: :job_event ])
+      .includes(:job_application_events, current_event: :job_application_event, job: [ current_event: :job_event ])
       .where.not(job: { current_event: { job_events: { type: "Job::Event::Deactivated" } } })
   end
 end
